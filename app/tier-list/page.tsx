36dd9bc2f722c9994fb0ec45fbd721b46d4e9b41
Sumit <sumit.kukreja@live.com>
2024-11-01 22:59:42 -0400

    Fix for missing file

--- app/tier-list/page.tsx
+++ app/tier-list/page.tsx
@@ -5,16 +5,50 @@ import Loading from "#/app/components/loading";
 import { Hero, Item, Team } from "#/graphql/generated/types";
 import { useHeroes } from "#/app/components/GetHeroesProvider";
 import MetaList from "./[uri]/MetaList";
-import Link from "next/link";
-import useAuth from "../components/UseAuth";
+import { getNonce } from "../components/GetNonce";
 
 export default function Page() {
   const { data: heroesData } = useHeroes();
-  const { loggedIn, loading } = useAuth();
-
+  
   useEffect(() => {
-    console.log("loggedIn", loggedIn);
-  }, [loggedIn, loading])
+    async function fetchUserData() {
+      // Fetch the nonce
+      const nonce = await getNonce();
+      if (!nonce) {
+        console.error("Failed to get nonce");
+        return;
+      }
+
+      try {
+        // Fetch user data from WordPress API using the nonce
+        const response = await fetch(
+          "https://api.heavenhold.com/wp-json/custom/v1/current-user",
+          {
+            method: "GET",
+            headers: {
+              "X-WP-Nonce": nonce,
+            },
+            credentials: "include",
+          }
+        );
+        console.log(response)
+        if (response.ok) {
+          const userData = await response.json();
+          console.log(userData)
+        } else {
+          console.error(
+            "Failed to fetch user data:",
+            response.status,
+            await response.text()
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
+
+    fetchUserData();
+  }, []);
 
   const heroes = useMemo(() => {
     return [...(heroesData?.heroes?.nodes ?? [])] as Hero[];

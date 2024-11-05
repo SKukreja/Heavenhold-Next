a8684e5ddf78c9c1be7669098e009b702b339a69
Sumit <sumit.kukreja@live.com>
2024-11-04 22:03:32 -0500

    Trying BatchHttpLink

--- app/components/ApolloWrapper.tsx
+++ app/components/ApolloWrapper.tsx
@@ -1,18 +1,21 @@
 // ApolloWrapper.tsx
 "use client";
 
-import { HttpLink } from "@apollo/client";
 import {
   ApolloNextAppProvider,
   ApolloClient,
   InMemoryCache,
 } from "@apollo/experimental-nextjs-app-support";
 
+import { BatchHttpLink } from "@apollo/client/link/batch-http";
+
 function makeClient() {
-  const httpLink = new HttpLink({
+  const httpLink = new BatchHttpLink({
     uri: "https://api.heavenhold.com/graphql",
     credentials: "include",
     fetchOptions: { cache: "no-store" },
+    batchMax: 5, // No more than 5 operations per batch
+    batchInterval: 20
   });
 
   return new ApolloClient({

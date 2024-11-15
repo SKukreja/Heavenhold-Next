e62c955260d8487e83ef7087758afae2884258f5
Sumit <sumit.kukreja@live.com>
2024-11-14 21:47:04 -0500

    Performance tweaks

--- app/api/auth/getIpAddress/route.ts
+++ app/api/auth/getIpAddress/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-    const ip = request.headers.get('x-forwarded-for') || '192.168.201.1'; // Fallback IP if not available
+    const ip = request.headers.get('x-forwarded-for') || '192.168.201.1';
     return NextResponse.json({ ip });
 }
--- ui/helpers.tsx
+++ ui/helpers.tsx
@@ -6,32 +6,15 @@ export function formatDate(dateString: string): string {
 
 // Function to get user's IP address for vote tracking
 export async function getIpAddress(): Promise<string> {
-  const delay = 2000;
-  let timeoutId: NodeJS.Timeout;
-  let cachedPromise: Promise<string> | null = null;
-
-  return new Promise<string>((resolve) => {
-      if (cachedPromise) {
-          resolve(cachedPromise);
-          return;
-      }
-
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(async () => {
-          cachedPromise = (async () => {
-              try {
-                  const response = await fetch("/api/getIpAddress");
-                  if (!response.ok) {
-                      throw new Error(`HTTP error! status: ${response.status}`);
-                  }
-                  const data = await response.json();
-                  return data.ip;
-              } catch (error) {
-                  console.error("Error fetching IP address:", error);
-                  return "192.168.201.1"; // Fallback IP address
-              }
-          })();
-          resolve(cachedPromise);
-      }, delay);
-  });
+    try {
+        const response = await fetch("/api/getIpAddress");
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data.ip;
+    } catch (error) {
+        console.error("Error fetching IP address:", error);
+        return "192.168.201.1"; // Fallback IP address
+    }
 }
